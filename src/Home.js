f65d0f0f17f5431eba2a7df308e7e579711e17f2
Anais <anaiszed447@gmail.com>
2022-03-25 11:21:50 -0400

    Created the content for the Home page.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,53 @@
 import React from "react";
+import Navbar from  './Navbar'
 
-function Home(){
+import './Home.css'
 
+const Page = (props) => {
     return(
-        <h1> Home Page</h1>
+        <> 
+         <div className="nav-point">
+             <img src={props.image}/>
+             <p> {props.name}</p>
+         </div>
+        </>
     )
 } 
 
+const Home = () =>{
 
-export default Home
+    return(
+            <>
+    
+            <div className="home-title">
+                <h1> Hello William Afton</h1>
+                <h2> Select Any of The Following Images</h2>
+            </div>
+            <div className="main-section">
+    
+                <div>
+                    <Page image="./images/game.png" name="Games" />
+                </div>
+                <div>
+                     <Page image="./images/quiz.png" name="Quizzes" />
+                </div>
+                <div>
+                     <Page image="./images/user_info.png" name="Profile" />
+                </div>
+                <div>
+                     <Page image="./images/parents.jpeg" name="Parent Help" />
+                </div>
+                <div>
+                     <Page image="./images/donation.png" name="Donate" />
+                </div>
+                <div>
+                     <Page image="./images/settings.png" name="Settings" />
+                </div>
+            </div>
+            
+            </>
+    )
+    
+}
+
+export default Home;
